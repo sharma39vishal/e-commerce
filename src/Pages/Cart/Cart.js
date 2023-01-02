7d576b7bc33f387385b9f54b930fa671658bb911
Kalash Jain <kalashjain446@gmail.com>
2023-01-02 21:52:42 +0530

    modified cart

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -39,8 +39,17 @@ export default function Cart() {
         }
     ]
     return (
-    
-        <div>
+        <>
+            <div className="container-fluid bg-secondary mb-5">
+                <div className="d-flex flex-column align-items-center justify-content-center" style={{minHeight:"300px"}}>
+                    <h1 className="font-weight-semi-bold text-uppercase mb-3">Shopping Cart</h1>
+                    <div className="d-inline-flex">
+                        <h2 className="m-0 "><a href="/home" className='text-danger'>Home</a></h2>
+                        <p className="m-0 px-2">-</p>
+                        <h2 className="m-0">Shopping Cart</h2>
+                    </div>
+                </div>
+            </div>
             <div className="container-fluid pt-5">
                 <div className="row px-xl-5">
                     <div className="col-lg-8 table-responsive mb-5">
@@ -64,41 +73,41 @@ export default function Cart() {
 
 
 
-                    <div class="col-lg-4">
-                        <form class="mb-5" action="">
-                            <div class="input-group">
-                                <input type="text" class="form-control p-4" placeholder="Coupon Code" />
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary">Apply Coupon</button>
+                    <div className="col-lg-4">
+                        <form className="mb-5" action="">
+                            <div className="input-group">
+                                <input type="text" className="form-control p-4" placeholder="Coupon Code" />
+                                <div className="input-group-append">
+                                    <button className="btn btn-primary">Apply Coupon</button>
                                 </div>
                             </div>
                         </form>
-                        <div class="card border-secondary mb-5">
-                            <div class="card-header bg-secondary border-0">
-                                <h4 class="font-weight-semi-bold m-0">Cart Summary</h4>
+                        <div className="card border-secondary mb-5">
+                            <div className="card-header bg-secondary border-0">
+                                <h4 className="font-weight-semi-bold m-0">Cart Summary</h4>
                             </div>
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between mb-3 pt-1">
-                                    <h6 class="font-weight-medium">Subtotal</h6>
-                                    <h6 class="font-weight-medium">$150</h6>
+                            <div className="card-body">
+                                <div className="d-flex justify-content-between mb-3 pt-1">
+                                    <h6 className="font-weight-medium">Subtotal</h6>
+                                    <h6 className="font-weight-medium">$150</h6>
                                 </div>
-                                <div class="d-flex justify-content-between">
-                                    <h6 class="font-weight-medium">Shipping</h6>
-                                    <h6 class="font-weight-medium">$10</h6>
+                                <div className="d-flex justify-content-between">
+                                    <h6 className="font-weight-medium">Shipping</h6>
+                                    <h6 className="font-weight-medium">$10</h6>
                                 </div>
                             </div>
-                            <div class="card-footer border-secondary bg-transparent">
-                                <div class="d-flex justify-content-between mt-2">
-                                    <h5 class="font-weight-bold">Total</h5>
-                                    <h5 class="font-weight-bold">$160</h5>
+                            <div className="card-footer border-secondary bg-transparent">
+                                <div className="d-flex justify-content-between mt-2">
+                                    <h5 className="font-weight-bold">Total</h5>
+                                    <h5 className="font-weight-bold">$160</h5>
                                 </div>
-                                <button class="btn btn-block btn-primary my-3 py-3">Proceed To Checkout</button>
+                                <button className="btn btn-block btn-primary my-3 py-3">Proceed To Checkout</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 
 }
